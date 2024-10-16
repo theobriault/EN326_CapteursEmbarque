c69603771651d7085f16c66238629c160085bd17
BRIAULT Theo <tbriault@yankee.pedago.ipb.fr>
2024-10-16 10:30:12 +0200

    Contrôle des GPIO LED1 et BUTTON1 de la Zest_Core_FMLR-72 avec ajout d'une interruption

--- main.cpp
+++ main.cpp
@@ -9,18 +9,53 @@
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
 
-
-int main()
-{
-    // Initialise the digital pin LED1 as an output
+// Initialise the digital pin LED1 as an output
 #ifdef LED1
     DigitalOut led(LED1);
 #else
     bool led;
 #endif
 
-    while (true) {
-        led = !led;
+
+// Initialise the digital pin BUTTON1 as an input
+#ifdef BUTTON1
+    //DigitalIn sw(BUTTON1);
+    InterruptIn sw(BUTTON1);
+#else
+    bool sw;
+#endif
+
+
+volatile bool button_clicked = false;  // Flag pour indiquer que l'interruption s'est produite
+
+
+void flip()
+{
+    led = !led;
+    button_clicked = true;
+}
+
+int main()
+{
+    sw.mode(PullDown);
+    sw.rise(&flip);
+
+    while (true)
+    {
+
+        if (button_clicked) {
+            printf("\nBouton pressé, LED inversée\n\n");
+            button_clicked = false; 
+        }
+
+        // Si utilisation de sw comme un DigitalIn
+        /*if (sw)
+            led = 1;
+        else
+            led = 0; */
+        
+
+        printf("hello world\n");
         ThisThread::sleep_for(BLINKING_RATE);
     }
 }
