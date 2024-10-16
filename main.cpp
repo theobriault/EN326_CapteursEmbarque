a457968f5516323df9c0a311fa7033686716331e
BRIAULT Theo <tbriault@yankee.pedago.ipb.fr>
2024-10-16 11:00:55 +0200

    Comptage et affichage à l'aide d'un timer

--- main.cpp
+++ main.cpp
@@ -25,37 +25,56 @@
     bool sw;
 #endif
 
-
+volatile bool count_finished = false;
 volatile bool button_clicked = false;  // Flag pour indiquer que l'interruption s'est produite
+using namespace std::chrono;    
+Timer t;
 
-
-void flip()
+void UpRoutine()
 {
     led = !led;
     button_clicked = true;
+    t.reset();
+    t.start();
+
+}
+
+void DownRoutine()
+{
+    t.stop();
+    count_finished = true;
 }
 
 int main()
 {
-    sw.mode(PullDown);
-    sw.rise(&flip);
+    //sw.mode(PullDown);
+    sw.rise(&UpRoutine);
+    sw.fall(&DownRoutine);
 
     while (true)
     {
-
-        if (button_clicked) {
-            printf("\nBouton pressé, LED inversée\n\n");
-            button_clicked = false; 
-        }
-
+        //Attente active (polling)
         // Si utilisation de sw comme un DigitalIn
         /*if (sw)
             led = 1;
         else
             led = 0; */
-        
 
-        printf("hello world\n");
+
+
+        if (button_clicked) 
+        {
+            printf("\nBouton pressé, LED inversée\n");
+            button_clicked = false; 
+        }
+                
+        if(count_finished)
+        {
+            printf("Duree appui : %llums\n", duration_cast<milliseconds>(t.elapsed_time()).count());
+            count_finished = false;
+        }
+  
+        //printf("hello world\n");
         ThisThread::sleep_for(BLINKING_RATE);
     }
 }
