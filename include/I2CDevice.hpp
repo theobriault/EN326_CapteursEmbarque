8c6f3d3c667534c61aafebc21367be15ef90e774
Théo Briault <theo.briault@bordeaux-inp.fr>
2024-11-19 13:09:55 +0100

    Add include

--- include/I2CDevice.hpp
+++ include/I2CDevice.hpp
@@ -4,4 +4,30 @@
 #include "mbed.h"
 
 
-class I2
+class I2CDevice {
+    public:
+        I2CDevice(PinName sda, PinName scl);
+
+        /*
+         *
+         */
+        bool write(uint8_t reg, const uint8_t* data, size_t length);
+
+
+        /*
+         *
+         */
+        bool read(uint8_t reg, uint8_t* buffer, size_t length);
+    
+    private:
+        I2C i2c;
+
+
+
+
+};
+
+
+
+
+#endif // I2CDEVICE_HPP
